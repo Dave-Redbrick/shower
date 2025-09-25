9cc12af858add02572bebae541b2c56eda9890be
Dave-Redbrick <dave@redbrick.space>
2025-09-25 14:02:51 +0900

    remove: old template

--- templates/title_only copy.js
+++
@@ -1,9 +0,0 @@
-import { marked } from 'marked';
-
-export default function createTitleOnlySlide(data) {
-  return `
-    <section class="slide">
-      <h2>${marked.parse(data.title)}</h2>
-    </section>
-  `;
-}
